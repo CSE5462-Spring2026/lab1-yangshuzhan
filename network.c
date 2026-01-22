8bbae412282035e17916afbbd849e3a4fef7b14e
Shuzhan Yang <53788376+yangshuzhan@users.noreply.github.com>
2026-01-22 17:15:53 -0500

    Add files via upload

--- network.c
+++ network.c
@@ -14,29 +14,55 @@
 #include <time.h>
 #include <unistd.h>
 #include <ctype.h>
+void format_message(char *buf) {
+    int in_quote = 0;
+
+    for (char *p = buf; *p; p++) {
+        if (*p == '"') {
+            in_quote = !in_quote;
+        } else if (*p == ' ' && !in_quote) {
+            *p = '\n';
+        }
+    }
+}
+
 int main(){
     int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-    int reuse = 1; // 定义一个变量，值为 1 (表示开启)
+    int reuse = 1; // 1 means on
     struct sockaddr_in src;
     socklen_t len = sizeof(src);
 
     struct sockaddr_in addr;
     struct ip_mreq mreq;
-    memset(&addr, 0, sizeof(addr));      // 清零，安全初始化
+    memset(&addr, 0, sizeof(addr));      // initialize
     addr.sin_family = AF_INET;            // IPv4
-    addr.sin_addr.s_addr = htonl(INADDR_ANY); // 绑定本机任意网卡
-    mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.1");
-    addr.sin_port = htons(1818);         // 端口号
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // address
+
+    char mcast_ip[32];
+    int port;
+
+    printf("Enter multicast IP port: ");
+    scanf("%31s%d", mcast_ip,&port);
+    mreq.imr_multiaddr.s_addr = inet_addr(mcast_ip); //multicast address 
+    addr.sin_port = htons(port);         //port number
 
     setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
     
     bind(socketfd, (struct sockaddr*)&addr, sizeof(addr));
     setsockopt(socketfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));//join
-    char buf[1024];  // 1 KB 的缓冲区
+    char buf[1024];  // 1 KB cache
 
-    int n =recvfrom(socketfd, buf, sizeof(buf), 0,(struct sockaddr*)&src, &len);
-    if (n > 0) {
-    buf[n] = 0; 
-    printf("received: %s\n", buf);
+    while (1) {
+    int n = recvfrom(socketfd, buf, sizeof(buf) - 1, 0,
+                     (struct sockaddr*)&src, &len);
+    if (n < 0) {
+        perror("recvfrom");
+        break;
     }
+
+    buf[n] = 0; 
+    format_message(buf);
+    printf("*****************\n %s\n", buf);
 }
+}
+
